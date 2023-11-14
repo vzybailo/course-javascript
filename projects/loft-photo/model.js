941177aaa16754752263161a87e149942d396b6c
Vital Zybailo01~ <vental@bk.ru>
2023-11-14 22:10:55 +0300

    project task 1

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -4,6 +4,21 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    let randomElem = null
+
+    for(let i = 0; i < array.length; i ++) {
+      let randomNum = Math.round(Math.random() * (array.length - 1))
+        randomElem = array[randomNum]
+    }
+  
+    return randomElem
+  },
+  getNextPhoto() {
+    const friend = this.getRandomElement(friendsDB)
+    const photos = photosDB[friend.id]
+    const photo = this.getRandomElement(photos)
+
+    return { friend, url: photo.url}
+  },
 };
